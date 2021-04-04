e90c052d686da6e749a4bede3edf9747be8aa805
Kenneth Abrahams Lartey <kyleabs20@gmail.com>
2021-04-04 22:45:33 +0000

    added header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import { getDefaultNormalizer } from "@testing-library/dom";
 import { React, Fragment } from "react";
 
+// components import 
+import Header from "./components/Header";
+
 const name = "Kenneth"
 const math = "yes"
 
 function App () {
     return (
       <Fragment>
+        <Header/>
         <h1>hello this is me </h1>
         <div>{name}</div>
         <p>performing calculations = { !math ? "2" : "3" }</p>
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import {React, Fragment} from 'react '
+
+function Header() {
+    return (
+        <Fragment>
+            <div>Task Tracker</div>
+        </Fragment>
+    )
+}
+
+export default Header
