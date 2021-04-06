885370fdca60a048d4037cc90388e55332a4d868
Kenneth Abrahams Lartey <kyleabs20@gmail.com>
2021-04-06 23:20:29 +0000

    added task array

--- src/components/task.js
+++ src/components/task.js
@@ -1,9 +1,30 @@
 import {React, Fragment} from 'react'
 
+const Task = [
+    {
+        id: 1, 
+        text: "Docktors Appointment", 
+        day: "Feb 5th at 2:30pm", 
+        reminder: true
+    }, 
+    {
+        id: 2, 
+        text: "Complete assigment ", 
+        day: "Feb 6th at 21:30pm", 
+        reminder: true
+    }, 
+    {
+        id: 3, 
+        text: "Meet your friends at Bani", 
+        day: "Feb 10th at 1:30pm", 
+        reminder: false
+    }
+]
+
 function Task(){
     return (
         <Fragment>
-            
+
         </Fragment> 
     )
 }
