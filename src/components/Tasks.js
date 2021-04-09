05d25420e198dab618c0d4e3323ba3564364077d
Kenneth Abrahams Lartey <kyleabs20@gmail.com>
2021-04-09 23:44:24 +0000

    working

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -2,7 +2,7 @@ import {FaTimes} from 'react-icons/fa'
 
 function Tasks ({tname, day, onDelete, id}){
     return (
-        <div className={"task"} onDoubleClick={()=>{}}>
+        <div className={"task"} onDoubleClick={()=>{console.log("hello")}}>
             <h3>{tname} <FaTimes style={{color: "red", cursor: "pointer"}} onClick={()=>{onDelete(id)}} /> </h3>
             <p>{day}</p>
         </div>
