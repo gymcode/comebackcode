73852a798f5b578691ccf6c81733df0511b5db8b
Kenneth Abrahams Lartey <kyleabs20@gmail.com>
2021-04-09 23:51:26 +0000

    linking to appjs

--- src/components/task.js
+++ src/components/task.js
@@ -1,13 +1,13 @@
 import {React, Fragment} from 'react'
 import Tasks from './Tasks'
 
-function Task({tasks, onDelete}){
+function Task({tasks, onDelete, toggleClass}){
 
     return (
         <Fragment>
             {tasks.map((items)=> (
                 <>
-                    <Tasks key={items.key} tname={items.text} day={items.day} id={items.id} onDelete={onDelete} toggleId={}/>
+                    <Tasks key={items.key} tname={items.text} day={items.day} id={items.id} onDelete={onDelete} toggleId={toggleClass}/>
                 </>
             ))}
         </Fragment> 
