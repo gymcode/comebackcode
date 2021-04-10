6c665bbd5a34969a25e90653c903777439f93c73
Kenneth Abrahams Lartey <kyleabs20@gmail.com>
2021-04-10 23:06:21 +0000

    sleepy?

---
+++ src/components/addTask.js
@@ -0,0 +1,24 @@
+import {Fragment} from 'react'
+
+function AddTask(){
+    return (
+        <Fragment>
+            <form className={"add-form"}> 
+                <div className={"form-control"}>
+                    <label>Task</label>
+                    <input type={"text"} placeholder={"Add Task"}/>
+                </div>
+                <div className={"form-control"}>
+                    <label>Day & time</label>
+                    <input type={"text"} placeholder={"Add Day & Time"}/>
+                </div>
+                <div className={"form-control"}>
+                    <label>Task</label>
+                    <input type={"text"} placeholder={"Add Task"}/>
+                </div>
+            </form>
+        </Fragment>
+    )
+}
+
+export default AddTask
