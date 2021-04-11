7a013ae54140c052c760a5b98707bb3290616a2a
Kenneth Abrahams Lartey <kyleabs20@gmail.com>
2021-04-11 00:15:21 +0000

    prevent default

--- src/components/addTask.js
+++ src/components/addTask.js
@@ -1,16 +1,21 @@
-import {Fragment} from 'react'
+import {Fragment, useState} from 'react'
 
 function AddTask(){
+
+    const [text, setText] = useState()
+    const [day, setDay] = useState()
+    const [reminder, setRemainder] = useState()
+
     return (
         <Fragment>
             <form className={"add-form"}> 
                 <div className={"form-control"}>
                     <label>Task</label>
-                    <input type={"text"} placeholder={"Add Task"}/>
+                    <input type={"text"} value={text} onChange={ (e) => setText(e.target.value) } placeholder={"Add Task"}/>
                 </div>
                 <div className={"form-control"}>
                     <label>Day & time</label>
-                    <input type={"text"} placeholder={"Add Day & Time"}/>
+                    <input type={"text"} value={text} onChange={ (e) => setDay(e.target.value) } placeholder={"Add Day & Time"}/>
                 </div>
                 <div className={"form-control form-control-check"}>
                     <label>Reminder</label>
